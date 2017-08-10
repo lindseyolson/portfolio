de9d072d0020287059c6dcebe77d5291c2fa762a
Lindsey Olson <lindseyolson00@gmail.com>
2017-08-09 19:34:48 -0500

    layout

--- server.js
+++ server.js
@@ -1,18 +1,20 @@
+// requires
 var express = require('express');
 var path = require('path');
 var app = express();
-
-// Using requst module to make HTTP requests from the server
-// https://www.npmjs.com/package/request
 var request = require('request');
+require('dotenv').config({path:'config.env'});
 
-// environment variables for Heroku
-var username = process.env.USER_NAME || require('./githubauth').username;
-var oauthToken = process.env.GIT_TOKEN || require('./githubauth').oauthToken;
-
+// environment variables
+var username = process.env.USERNAME;
+var oauthToken = process.env.OAUTH_TOKEN;
 var port = process.env.PORT || 1616;
 
 app.use(express.static('app'));
+
+app.get('/', function(req, res) {
+  res.sendFile(path.join(__dirname, 'app/views/index.html'));
+});
 
 var project_options = {
   url: 'https://api.github.com/users/' + username,
@@ -22,7 +24,6 @@ var project_options = {
   }
 };
 
-// Moved API call into server to protect oAuthToken
 app.get('/projects', function(req, res){
   request(project_options, function (error, response, body) {
     if (response && response.statusCode == 200) {
@@ -41,7 +42,6 @@ var repo_options = {
   }
 };
 
-// Moved API call into server to protect oAuthToken
 app.get('/repos', function(req, res){
   request(repo_options, function (error, response, body) {
     if (response && response.statusCode == 200) {
@@ -52,9 +52,7 @@ app.get('/repos', function(req, res){
   });
 });
 
-app.get('/', function(req, res) {
-  res.sendFile(path.join(__dirname, 'app/views/index.html'));
-});
+
 
 app.listen(port, function() {
   console.log('listening on port:', port);

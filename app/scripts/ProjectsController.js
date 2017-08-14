abc5f5a41ce38a08ab2b8c2892801c7afc0a2eef
Lindsey Olson <lindseyolson00@gmail.com>
2017-08-14 07:29:04 -0500

    working on function for ng-show/hide

--- app/scripts/ProjectsController.js
+++ app/scripts/ProjectsController.js
@@ -3,6 +3,21 @@ myApp.controller('ProjectsController', ProjectsController);
 function ProjectsController(GithubAPI) {
   var vm = this;
 
+  // vm.showProject = function() {
+  //
+  // };
+
+  vm.showIfClicked = function(type) {
+    console.log(type);
+    type = true;
+    console.log(type);
+  };
+
+  // vm.showPortfolio = vm.showIfClicked;
+
+
+
+
   GithubAPI.githubRepos().then(function(response){
     vm.projects = response;
   }); // end githubRepos

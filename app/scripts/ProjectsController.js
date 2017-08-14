729e7e9135e180ae2687b591c2b71fdf6fcf29d6
Lindsey Olson <lindseyolson00@gmail.com>
2017-08-14 18:52:45 -0500

    full page web page working

--- app/scripts/ProjectsController.js
+++ app/scripts/ProjectsController.js
@@ -1,24 +1,75 @@
-myApp.controller('ProjectsController', ProjectsController);
+  myApp.controller('ProjectsController', ProjectsController);
 
-function ProjectsController(GithubAPI) {
-  var vm = this;
-
-  // vm.showProject = function() {
-  //
-  // };
-
-  vm.showIfClicked = function(type) {
-    console.log(type);
-    type = true;
-    console.log(type);
-  };
-
-  // vm.showPortfolio = vm.showIfClicked;
+  function ProjectsController(GithubAPI) {
+    var vm = this;
+    vm.showPortfolio = false;
+    vm.showFlora = false;
+    vm.showPraeco = false;
+    vm.showTodo = false;
+    vm.showBlocks = false;
+    vm.showGallery = false;
 
 
 
+    vm.showProject = function(project) {
+      switch (project) {
+        case 'portfolioTN':
+          vm.showFlora = false;
+          vm.showPraeco = false;
+          vm.showTodo = false;
+          vm.showBlocks = false;
+          vm.showGallery = false;
+          vm.showPortfolio = true;
+          break;
+        case 'floraTN':
+          vm.showPraeco = false;
+          vm.showTodo = false;
+          vm.showBlocks = false;
+          vm.showGallery = false;
+          vm.showPortfolio = false;
+          vm.showFlora = true;
+          break;
+        case 'praecoTN':
+          vm.showTodo = false;
+          vm.showBlocks = false;
+          vm.showGallery = false;
+          vm.showPortfolio = false;
+          vm.showFlora = false;
+          vm.showPraeco = true;
+          break;
+        case 'todo-listTN':
+          vm.showBlocks = false;
+          vm.showGallery = false;
+          vm.showPortfolio = false;
+          vm.showFlora = false;
+          vm.showPraeco = false;
+          vm.showTodo = true;
+          break;
+        case 'blocksTN':
+          vm.showGallery = false;
+          vm.showPortfolio = false;
+          vm.showFlora = false;
+          vm.showPraeco = false;
+          vm.showTodo = false;
+          vm.showBlocks = true;
+          break;
+        case 'galleryTN':
+          vm.showPortfolio = false;
+          vm.showFlora = false;
+          vm.showPraeco = false;
+          vm.showTodo = false;
+          vm.showBlocks = false;
+          vm.showGallery = true;
+          break;
+        } // end switch statement
+        scroll(0,0);
+      }; // end show project
 
-  GithubAPI.githubRepos().then(function(response){
-    vm.projects = response;
-  }); // end githubRepos
-} // end ProjectsPageController
+
+
+
+
+    GithubAPI.githubRepos().then(function(response){
+      vm.projects = response;
+    }); // end githubRepos
+  } // end ProjectsPageController
